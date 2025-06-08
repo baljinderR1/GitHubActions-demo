ffd21569562e2421ab7815c8bd2be8b463f55330
baljinderR1 <baljinder_k11@hotmail.com>
2025-06-08 17:03:24 -0400

    Edit some code in sample test

--- cypress/e2e/sample.spec.cy.js
+++ cypress/e2e/sample.spec.cy.js
@@ -2,9 +2,10 @@
 describe('Sample Test', () =>{
 
 
-    it('My very first test ', ()=>{  
+    it('My very first test1 ', ()=>{  
    
     cy.visit('https://linkedin.com');
     cy.viewport('macbook-15');
+    cy.log('Hello how are you ?')
     });
 });
